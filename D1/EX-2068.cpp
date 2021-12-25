3101548c6fff318219cbf17c1016ba002265970b
Codetesting <37256601+Codetesing@users.noreply.github.com>
2021-12-25 16:59:34 +0900

    최대수 구하기
    최대수 구하기

---
+++ D1/EX-2068.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T, N = 10;
+	int tmp;
+
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int max = -1;
+		for (int i = 0; i < N; i++)
+		{
+			scanf("%d", &tmp);
+			if (max < tmp)
+				max = tmp;
+		}
+		
+		printf("#%d %d\n", test_case, max);
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
