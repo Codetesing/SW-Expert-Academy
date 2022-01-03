dbd78974927789be713f750a28f6d3404bcbdf50
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-01-03 14:47:28 +0900

    간단한 소인수분해
    간단한 소인수분해

---
+++ D2/EX-1945.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	int DIV[] = { 2, 3, 5, 7, 11 };
+	
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N;
+		int MOD[] = { 0, 0, 0, 0, 0 };
+		cin >> N;
+
+		for (int i = 0; i < 5; i++)
+			while (N % DIV[i] == 0)
+			{
+				MOD[i]++;
+				N /= DIV[i];
+			}
+
+		printf("#%d ", test_case);
+		for (int i = 0; i < 5; i++)
+			printf("%d ", MOD[i]);
+		printf("\n");
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
