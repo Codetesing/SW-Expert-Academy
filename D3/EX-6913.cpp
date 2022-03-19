a2777465b67b0a2ae9fdea3e256d0eb6c419d92f
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-03-19 20:03:51 +0900

    동철이의 프로그래밍 대회
    동철이의 프로그래밍 대회

---
+++ D3/EX-6913.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N, M; cin >> N >> M;
+
+		vector<int> score(N);
+
+		int temp;
+		for (int i = 0; i < N; i++)
+		{
+			int cnt = 0;
+			for (int j = 0; j < M; j++)
+			{
+				scanf("%d", &temp);
+				if (temp == 1)
+					cnt++;
+			}
+			score[i] = cnt;
+		}
+		
+		int max = *max_element(score.begin(), score.end());
+		int grade = 0;
+
+		for (int i = 0; i < N; i++)
+			if (score[i] == max)
+				grade++;
+
+		printf("#%d %d %d\n", test_case, grade, max);
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
