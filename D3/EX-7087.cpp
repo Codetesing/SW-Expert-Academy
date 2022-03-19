1d97d0f8b0afdb0cf43702fdb7829e93446417a3
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-03-19 19:55:10 +0900

    문제 제목 붙이기
    문제 제목 붙이기

---
+++ D3/EX-7087.cpp
@@ -0,0 +1,37 @@
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N; cin >> N;
+		bool visited[26] = { false };
+
+		vector<string> in(N);
+		for (int i = 0; i < N; i++)
+			cin >> in[i];
+
+		for (int i = 0; i < N; i++)
+			visited[in[i][0] - 'A'] = true;
+
+		int out = 0;
+		for (int i = 0; i < 26; i++, out++)
+			if (!visited[i])
+				break;
+
+		printf("#%d %d\n", test_case, out);
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
