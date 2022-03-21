baecc8b1c1acc3e2b1a92c352f48bdf9503cb6f5
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-03-21 09:42:48 +0900

    다솔이의 월급 상자
    다솔이의 월급 상자

---
+++ D3/EX-6692.cpp
@@ -0,0 +1,32 @@
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N; cin >> N;
+		vector<double> P(N);
+		vector<int> X(N);
+		double out = 0;
+
+		for (int i = 0; i < N; i++)
+			cin >> P[i] >> X[i];
+
+		for (int i = 0; i < N; i++)
+			out += P[i] * X[i];
+
+		printf("#%d %lf\n", test_case, out);
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
