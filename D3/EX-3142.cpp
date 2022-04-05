c4b5538c9ab4f1394e2b1489840910368f261057
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-04-05 16:44:34 +0900

    영준이와 신비한 뿔의 숲
    영준이와 신비한 뿔의 숲

---
+++ D3/EX-3142.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N, M; cin >> N >> M;
+		int UNI, TWIN;
+
+		TWIN = N - M;
+		UNI = M - TWIN;
+
+		printf("#%d %d %d\n", test_case, UNI, TWIN);
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
